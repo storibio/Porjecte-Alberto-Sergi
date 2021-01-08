486fbaaf933844027ce2fdd4a6595b5176ff6c7f
storibioblasco <s.toribio@sapalomera.cat>
2021-01-08 19:55:18 +0100

    Carregar taulell

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("EEEEEEAAAAAAAAA");
+        int i=0;
+        while (i==0){
+            System.out.println("Menu:");
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            //Opcio 0//
+            if (num == 0){
+                i++;
+            }
+            //Opcio1//
+            if (num==1){
+                int rows = sc.nextInt();
+                int columns = sc.nextInt();
+                int[][] array = new int[rows][columns];
+                System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+            }
+        }
     }
 }

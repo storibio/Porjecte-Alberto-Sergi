17476975b8abd1e50d01289556b5155bd1af9741
alberto <a.bermejo@sapalomera.cat>
2021-01-08 19:55:25 +0100

    Opcio 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,29 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
-        while (i == 0) {
-            System.out.println("Menu:");
-            Scanner opcio = new Scanner(System.in);
-            int num = opcio.nextInt();
+        int i=0;
+        while (i==0){
+        System.out.println("Menu:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
             //Opcio 0//
-            if (num == 0) {
-                i++;
-            }
-            //Opcio1//
-            if (num == 1) {
-                System.out.println("ee");
-            }
+        if (num == 0){
+           i++;
+        }
+        //Opcio1//
+        if (num==1){
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+            int[][] array = new int[rows][columns];
+            System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
 
         }
+        }
+
     }
 }

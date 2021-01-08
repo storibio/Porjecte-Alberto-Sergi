f7c975a26965a8d8a6486a216b90c3631d3dde60
alberto <a.bermejo@sapalomera.cat>
2021-01-08 19:13:31 +0100

    Menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("EEEEEEAAAAAAAAA");
+        int i = 0;
+        while (i == 0) {
+            System.out.println("Menu:");
+            Scanner opcio = new Scanner(System.in);
+            int num = opcio.nextInt();
+            //Opcio 0//
+            if (num == 0) {
+                i++;
+            }
+            //Opcio1//
+            if (num == 1) {
+                System.out.println("ee");
+            }
+
+        }
     }
 }

39c7f488b519df6b79484ddde3b0569d41f45b83
storibioblasco <s.toribio@sapalomera.cat>
2021-01-09 20:29:54 +0100

    op2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,43 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
-        int[][] array;
-        int inputrow = 0;
-        int inputcol = 0;
-        int replace = 0;
-        while (i == 0) {
+        int i=0;
+        int[][] array = new int[0][0];
+        while (i==0){
             System.out.println("Menu:");
             Scanner sc = new Scanner(System.in);
             int num = sc.nextInt();
             //Opcio 0//
-            if (num == 0) {
+            if (num == 0){
                 i++;
             }
             //Opcio1//
-            if (num == 1) {
+            else if (num==1) {
                 int rows = sc.nextInt();
                 int columns = sc.nextInt();
                 array = new int[rows][columns];
                 System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
             }
-            //Opcio2//
-            if (num == 2) {
+            else if (num == 2) {
                 int rows = sc.nextInt();
                 int columns = sc.nextInt();
-                array = new int[rows][columns];
-                System.out.println("Enter Row: ");
-                inputrow = sc.nextInt();
-                System.out.println("Enter Cols:");
-                inputcol = sc.nextInt();
-                System.out.println("Enter New Number: ");
-                replace = 1;
-                array[inputrow][inputcol] = replace;
+                columns--;
+                rows--;
+                array[rows][columns] = 1;
                 System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
             }
-            //Opcio 0//
-            if (num == 0) {
-                i++;
+            else if (num == 6) {
+                System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
             }
         }
     }

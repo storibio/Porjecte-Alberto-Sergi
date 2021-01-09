9524560da6d4c7f6e7d0878cab4c0a76d0ed6a2c
storibioblasco <s.toribio@sapalomera.cat>
2021-01-09 20:03:26 +0100

    op2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("EEEEEEAAAAAAAAA");
+        int i = 0;
+        int[][] array;
+        int inputrow = 0;
+        int inputcol = 0;
+        int replace = 0;
+        while (i == 0) {
+            System.out.println("Menu:");
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            //Opcio 0//
+            if (num == 0) {
+                i++;
+            }
+            //Opcio1//
+            if (num == 1) {
+                int rows = sc.nextInt();
+                int columns = sc.nextInt();
+                array = new int[rows][columns];
+                System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+            }
+            //Opcio2//
+            if (num == 2) {
+                int rows = sc.nextInt();
+                int columns = sc.nextInt();
+                array = new int[rows][columns];
+                System.out.println("Enter Row: ");
+                inputrow = sc.nextInt();
+                System.out.println("Enter Cols:");
+                inputcol = sc.nextInt();
+                System.out.println("Enter New Number: ");
+                replace = 1;
+                array[inputrow][inputcol] = replace;
+                System.out.println(Arrays.deepToString(array).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+            }
+            //Opcio 0//
+            if (num == 0) {
+                i++;
+            }
+        }
     }
 }
